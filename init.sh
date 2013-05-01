c4cc37b06869536e020f2f138922af8f290eeaca
Brian Vallelunga <bvallelunga@parkme.com>
2013-05-01 10:46:37 -0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -24,6 +24,7 @@ done
 
 clear;
 adduser --force-badname $username;
+htpasswd -b /home/.htpasswd $username $password
 mysql --user="$mysql_username" --password="$mysql_password" -e "CREATE USER '$username'@'localhost' IDENTIFIED BY '$password'"
 chmod 0750 /home/$username
 chown -R $username /home/$username

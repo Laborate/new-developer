a881040a2bde4efef597313f4883178dd95dd611
root <root@laborate-dev.(none)>
2013-05-01 17:27:21 +0000

    update

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@ while [[ -z "$mysql_username" || -z "$mysql_password" ]]; do
     clear;
     read -p "MYSQL Username: " mysql_username;
     read -p "MYSQL Password: " mysql_password;
-    read -p "Is this information correct [y,n]: " mysql_correct;
+    read -p "Is this information correct [Y/n]: " mysql_correct;
 
     if [ "$mysql_correct" == "n" ]; then
         mysql_username=;
@@ -14,7 +14,7 @@ while [[ -z "$username" || -z "$password" ]]; do
     clear;
     read -p "New Username: " username;
     read -p "New Password: " password;
-    read -p "Is this information correct [y,n]: " user_correct;
+    read -p "Is this information correct [Y/n]: " user_correct;
 
     if [ "$user_correct" == "n" ]; then
         username=;
@@ -26,5 +26,5 @@ clear;
 adduser --force-badname $username;
 mysql --user="$mysql_username" --password="$mysql_password" -e "CREATE USER '$username'@'localhost' IDENTIFIED BY '$password'"
 chmod 0750 /home/$username
-ALL ALL=NOPASSWD: /home/*/*/init/init.sh
+chown -R $username /home/$username
 rm -R "$(cd "$(dirname "$0")"; pwd)/../new_developer";

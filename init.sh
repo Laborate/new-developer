e877ec4db3ba0dc8fa06cbc91e395c93bf87638e
Brian Vallelunga <bvallelunga@parkme.com>
2013-05-01 22:05:03 +0000

    update

--- init.sh
+++ init.sh
@@ -10,8 +10,10 @@ while [[ -z "$mysql_username" || -z "$mysql_password" ]]; do
     fi
 done
 
-while [[ -z "$username" || -z "$password" ]]; do
+while [[ -z "$username" || -z "$password" || -z "$name" || -z "$email" ]]; do
     clear;
+    read -p "New Full Name: " name;
+    read -p "Github Profile Email: " email;
     read -p "New Username: " username;
     read -p "New Password: " password;
     read -p "Is this information correct [Y/n]: " user_correct;
@@ -19,20 +21,25 @@ while [[ -z "$username" || -z "$password" ]]; do
     if [ "$user_correct" == "n" ]; then
         username=;
         password=;
+        name=;
+        email=;
     fi
 done
 
 clear;
 adduser --force-badname $username;
-htpasswd -b /home/.htpasswd $username $password
-mysql --user="$mysql_username" --password="$mysql_password" -e "CREATE USER '$username'@'localhost' IDENTIFIED BY '$password'"
+htpasswd -b /home/.htpasswd $username $password;
+mysql --user="$mysql_username" --password="$mysql_password" -e "CREATE USER '$username'@'localhost' IDENTIFIED BY '$password'";
 
-chmod 751 /home/$username
+chmod 751 /home/$username;
 
-su -c "git config --global color.ui auto" - $username
-su -c "git config --global core.editor 'vim'" - $username
-su -c "git config --global merge.tool vimdiff" - $username
-su -c "export VISUAL=vim" - $username
-su -c "export EDITOR=vim" - $username
+su -c "git config --global user.name '$name'" - $username;
+su -c "git config --global user.email '$email'" - $username;
+
+su -c "git config --global color.ui auto" - $username;
+su -c "git config --global core.editor 'vim'" - $username;
+su -c "git config --global merge.tool vimdiff" - $username;
+su -c "export VISUAL=vim" - $username;
+su -c "export EDITOR=vim" - $username;
 
 rm -R "$(cd "$(dirname "$0")"; pwd)/../new_developer";
